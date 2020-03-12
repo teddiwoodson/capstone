e82d67cf7cd5a868f06f7d66875d534ee0898351
Teddi Woodson <teddiwood97@ufl.edu>
2020-03-11 22:15:03 -0400

    Add header to products and story pages

---
+++ page-story.php
@@ -0,0 +1,17 @@
+<?php
+/*
+    Template Name: story
+    Template Type: Page
+*/
+ ?>
+
+<?php get_header(); ?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+    <?php dynamic_sidebar('hero-image'); ?>
+  </div>
+</div>
+
+<?php get_footer(); ?>
