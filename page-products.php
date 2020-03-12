f6050022fec92bda971f15bff601de9f65e06c01
Teddi Woodson <teddiwood97@ufl.edu>
2020-03-11 22:33:14 -0400

    Add tiles to products page

--- page-products.php
+++ page-products.php
@@ -13,5 +13,67 @@
     <?php dynamic_sidebar('hero-image'); ?>
   </div>
 </div>
+</div>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="content col-md-12">
+            <div class="grid">
+                <figure class="effect-chico">
+                    <img src="https://tympanus.net/Development/HoverEffectIdeas/img/15.jpg" alt="img15"/>
+                    <figcaption>
+                        <h2>Title</h2>
+                        <p>Exerpt</p>
+                        <a href="Link">View more</a>
+                      </figcaption>
+                    </figure>
+                  <figure class="effect-chico">
+                      <img src="https://tympanus.net/Development/HoverEffectIdeas/img/15.jpg" alt="img15"/>
+                      <figcaption>
+                          <h2>Title</h2>
+                          <p>Exerpt</p>
+                          <a href="Link">View more</a>
+                        </figcaption>
+                      </figure>
+                    <figure class="effect-chico">
+                        <img src="https://tympanus.net/Development/HoverEffectIdeas/img/15.jpg" alt="img15"/>
+                        <figcaption>
+                            <h2>Title</h2>
+                            <p>Exerpt</p>
+                            <a href="Link">View more</a>
+                          </figcaption>
+                        </figure>
+                        <figure class="effect-chico">
+                            <img src="https://tympanus.net/Development/HoverEffectIdeas/img/15.jpg" alt="img15"/>
+                            <figcaption>
+                                <h2>Title</h2>
+                                <p>Exerpt</p>
+                                <a href="Link">View more</a>
+                              </figcaption>
+                            </figure>
+                          <figure class="effect-chico">
+                              <img src="https://tympanus.net/Development/HoverEffectIdeas/img/15.jpg" alt="img15"/>
+                              <figcaption>
+                                  <h2>Title</h2>
+                                  <p>Exerpt</p>
+                                  <a href="Link">View more</a>
+                                </figcaption>
+                              </figure>
+                            <figure class="effect-chico">
+                                <img src="https://tympanus.net/Development/HoverEffectIdeas/img/15.jpg" alt="img15"/>
+                                <figcaption>
+                                    <h2>Title</h2>
+                                    <p>Exerpt</p>
+                                    <a href="Link">View more</a>
+                                  </figcaption>
+                                </figure>
+                  </div>
+
+          </div>
+
+    </div>
+  </div>
+
+
 
 <?php get_footer(); ?>
