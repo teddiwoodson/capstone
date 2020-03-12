bff62743e1928e5329054f1064f7278a0c1d99c2
Teddi Woodson <teddiwood97@ufl.edu>
2020-03-11 22:13:27 -0400

    Update inspiration template with header and post grid

---
+++ page-inspo.php
@@ -0,0 +1,41 @@
+<?php
+/*
+    Template Name: Inspiration
+    Template Type: Page
+*/
+ ?>
+
+<?php get_header(); ?>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+    <?php dynamic_sidebar('hero-image'); ?>
+  </div>
+</div>
+
+<div class="container">
+    <div class="row justify-content-center">
+      <?php if(have_posts()){
+        while(have_posts()){
+          the_post(); ?>
+                <div class="content">
+                    <div class="grid">
+                        <figure class="effect-chico">
+                          <?php  the_post_thumbnail('medium'); ?>
+                          <figcaption>
+                            <h2><?php the_title(); ?></h2>
+                            <p><?php the_excerpt(); ?></p>
+                            <a href="<?php the_permalink(); ?>">View more</a>
+                          </figcaption>
+                        </figure>
+                    </div>
+                </div>
+
+        <?php } // ends while loop
+      } // ends if statement
+      ?>
+    </div>
+  </div>
+
+<?php get_footer(); ?>
