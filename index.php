c671c1cf887f81d3d6e0bf02342db1a493a1bc99
Teddi Woodson <teddiwood97@ufl.edu>
2020-03-11 22:13:10 -0400

    Update home page to home template

--- index.php
+++ index.php
@@ -7,4 +7,28 @@
   </div>
 </div>
 
+<div class="container">
+    <div class="row justify-content-center">
+      <?php if(have_posts()){
+        while(have_posts()){
+          the_post(); ?>
+                <div class="content">
+                    <div class="grid">
+                        <figure class="effect-chico">
+                          <?php  the_post_thumbnail('medium'); ?>
+                          <figcaption>
+                            <h2><?php the_title(); ?></h2>
+                            <p><?php the_excerpt(); ?></p>
+                            <a href="<?php the_permalink(); ?>">View more</a>
+                          </figcaption>
+                        </figure>
+                    </div>
+                </div>
+
+        <?php } // ends while loop
+      } // ends if statement
+      ?>
+    </div>
+  </div>
+
 <?php get_footer(); ?>
