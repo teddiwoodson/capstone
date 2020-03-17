0b3df04d4bafa50bd42881b1d2b0b0c5f211bb41
Teddi Woodson <teddiwood97@ufl.edu>
2020-03-16 20:00:19 -0400

    Add initial styles to the single.php page

--- single.php
+++ single.php
@@ -1,3 +1,21 @@
 <?php get_header(); ?>
+  <div class="container">
+    <div class="row justify-content-center">
+      <main class="col-md-10">
+        <?php
+          if(have_posts()) {
+            while(have_posts()){
+              the_post(); ?>
+              <h2 class="entry-title"><?php the_title(); ?></h2>
+              <!--<p><?php echo "<span class='bold'>Published </span>" . get_the_date(); echo " | "; echo "<span class='bold'>Written by </span>". get_the_author();  ?></p>
 
+              <?php the_content(); ?>-->
+          <?php  } //this ends while loop
+          }// this ends if statement
+        ?>
+
+        <a class="btn btn-secondary btn-sm" href="<?php echo get_home_url(); ?>">Return to Home Page</a>
+      </main>
+    </div>
+  </div>
 <?php get_footer(); ?>
