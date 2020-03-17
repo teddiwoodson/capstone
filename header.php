c840fbf9f2f4a84d487c416ca71c51f81219d61e
Teddi Woodson <teddiwood97@ufl.edu>
2020-03-17 11:26:54 -0400

    Add mobile responsiveness to navigation

--- header.php
+++ header.php
@@ -14,9 +14,9 @@
 
 <body>
 <header>
+  <div class="desktop-nav">
     <div class="container">
-      <div class="row">
-        <div class="d-flex justify-content-around align-items-baseline">
+      <div class="row d-flex justify-content-around align-items-baseline">
           <div class="col-3">
             <?php if(get_header_image() == '') {?>
                 <h1><a href="<?php get_home_url();?>"><?php bloginfo('name');?></a></h1><?php
@@ -25,18 +25,41 @@
 
               <?php } ?>
           </div>
-          <div class="col-9">
-            <nav>
-              <?php
-                if(has_nav_menu('top-menu')) {
-                  wp_nav_menu(array('theme_location' => 'top-menu', 'container_class' => 'top-menu-class'));
-                } else{
-                  echo "Please select a top menu through the dashboard";
-                }
-              ?>
-            </nav>
+            <div class="col-9 justify-content-end">
+              <nav>
+                <?php
+                  if(has_nav_menu('top-menu')) {
+                    wp_nav_menu(array('theme_location' => 'top-menu', 'container_class' => 'top-menu-class'));
+                  } else{
+                    echo "Please select a top menu through the dashboard";
+                  }
+                ?>
+              </nav>
+            </div>
           </div>
-        </div>
       </div>
     </div>
+
+    <div class="mobile-nav">
+      <div class="container">
+        <div class="row justify-content-center">
+          <?php if(get_header_image() == '') {?>
+              <h1><a href="<?php get_home_url();?>"><?php bloginfo('name');?></a></h1><?php
+            }else {?>
+              <a href="<?php get_home_url();?>"><img src="<?php header_image();?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" atl="logo" /></a>
+
+            <?php } ?>
+        </div>
+        <nav>
+          <?php
+            if(has_nav_menu('top-menu')) {
+              wp_nav_menu(array('theme_location' => 'top-menu', 'container_class' => 'top-menu-class'));
+            } else{
+              echo "Please select a top menu through the dashboard";
+            }
+          ?>
+        </nav>
+  </div>
+      </div>
+
   </header>
