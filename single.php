ea9a98f7823d1740eff4cb8d1ba322c1776f0d2e
Teddi Woodson <teddiwood97@ufl.edu>
2020-03-16 21:31:17 -0400

    Add styles and sidebar to single.php

--- single.php
+++ single.php
@@ -1,21 +1,25 @@
 <?php get_header(); ?>
   <div class="container">
-    <div class="row justify-content-center">
-      <main class="col-md-10">
+    <div class="row justify-content-center" id="posts">
+      <main class="col-md-9">
         <?php
           if(have_posts()) {
             while(have_posts()){
               the_post(); ?>
-              <h2 class="entry-title"><?php the_title(); ?></h2>
-              <!--<p><?php echo "<span class='bold'>Published </span>" . get_the_date(); echo " | "; echo "<span class='bold'>Written by </span>". get_the_author();  ?></p>
+              <h3 class="entry-title"><?php the_title(); ?></h3>
+              <p><?php echo "<span class='bold'>Published </span>" . get_the_date(); ?></p>
 
-              <?php the_content(); ?>-->
+              <?php the_content(); ?>
           <?php  } //this ends while loop
           }// this ends if statement
         ?>
 
         <a class="btn btn-secondary btn-sm" href="<?php echo get_home_url(); ?>">Return to Home Page</a>
       </main>
+
+      <aside class="col-md-3">
+        <?php get_sidebar(); ?>
+      </aside>
     </div>
   </div>
 <?php get_footer(); ?>
