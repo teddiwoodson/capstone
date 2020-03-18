8cdbb7dc07c798ad81d62c2cd762ce65774a6706
Teddi Woodson <teddiwood97@ufl.edu>
2020-03-18 13:25:00 -0400

    Edit styles to contact page and add filler image

--- page-contact.php
+++ page-contact.php
@@ -13,12 +13,14 @@
     <h2 class="p-4">Contact Us</h2>
   </section>
 <!-- ======================= contact form and photo area ==================-->
-  <section class="row">
+  <section class="row justify-content around">
     <div class="col-md-6 d-flex justify-content-center">
       <?php dynamic_sidebar('contact-form'); ?>
     </div>
     <div class="col-md-6 d-flex justify-content-center">
-      <?php dynamic_sidebar('contact-image'); ?>
+      <div class="contact-img">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/contact.jpg" alt="contact us" title="contact us"/>
+      </div>
     </div>
   </section>
 </main>
@@ -30,26 +32,26 @@
       <div class="col-md-4 text-center">
         <img class="faq" src="<?php echo get_template_directory_uri(); ?>/images/retailer.png" alt="retailer" title="retailer"/>
         <p>Become a Retailer</p>
-        <!----BUTTON FOR FAKE LINK-->
+        <!--BUTTON FOR FAKE LINK-->
         <a href="http://unbouncepages.com/ccp-wholesale/" class="btn btn-primary" target="blank" role="button">Click Here</a>
       </div>
       <div class="col-md-4 text-center">
         <img class="faq" src="<?php echo get_template_directory_uri(); ?>/images/faq.png" alt="faq" title="faq"/>
         <p>FAQ</p>
-        <!----BUTTON FOR ACTUAL LINK-->
+        <!--BUTTON FOR ACTUAL LINK-->
         <a href="https://www.countrychicpaint.com/apps/help-center" class="btn btn-primary" target="blank" role="button">Click Here</a>
       </div>
       <div class="col-md-4 text-center">
         <img class="faq" src="<?php echo get_template_directory_uri(); ?>/images/represent.png" alt="represent" title="represent"/>
         <p>Live Chat</p>
-        <!----BUTTON FOR ACTUAL LINK-->
+        <!--BUTTON FOR ACTUAL LINK-->
         <a href="https://www.countrychicpaint.com/pages/contact-us" class="btn btn-primary" target="blank" role="button">Click Here</a>
     </div>
   </section>
 
 <!-- ======================= Map and Contact Info  ==================-->
 
-<section class="container">
+<section class="container" id="map-and-contact">
   <section class="row">
     <div class="col-md-6 d-flex justify-content-center">
       <?php dynamic_sidebar('map'); ?>
