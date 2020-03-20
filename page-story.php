67926ecd46969c7d70ac2379d8ab6a115ac9bec0
Elizabeth Tran <elizabethtran135@gmail.com>
2020-03-19 23:30:43 -0400

    Remove story copy and contact click here button

--- page-story.php
+++ page-story.php
@@ -23,22 +23,18 @@
       <div class="col-md-3 text-center" id="value">
         <img src="<?php echo get_template_directory_uri(); ?>/images/environmental.png" alt="environmental impact" title="environmental impact"/>
         <h4>Low environmental impact</h4>
-        <p>We pride ourselves on using renewable energy to create VOC-free products.</p>
       </div>
       <div class="col-md-3 text-center" id="value">
         <img class="value" src="<?php echo get_template_directory_uri(); ?>/images/recycle.png" alt="recycle" title="recycle"/>
         <h4>Participating in Recycling Programs</h4>
-        <p>We are a proud member of paint recycling programs across North America to ensure unused paints are recycled.</p>
       </div>
       <div class="col-md-3 text-center" id="value">
         <img class="value" src="<?php echo get_template_directory_uri(); ?>/images/house.png" alt="house" title="house"/>
         <h4>Supporting the local economy</h4>
-        <p>Our products and raw materials are sourced and manufactured in North America whenever possible.</p>
       </div>
       <div class="col-md-3 text-center" id="value">
         <img class="value"src="<?php echo get_template_directory_uri(); ?>/images/heart.png" alt="heart" title="heart"/>
         <h4>Helping those in need</h4>
-        <p>A portion of our profits are donated to help provide meals to families in North America.</p>
       </div>
     </div>
   </section>
