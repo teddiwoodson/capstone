0504a3cdce1cb94b1db11d92a8a5c0b79d8c10f6
Elizabeth Tran <elizabethtran135@gmail.com>
2020-03-20 00:17:57 -0400

    Link values text and images

--- page-story.php
+++ page-story.php
@@ -16,25 +16,25 @@
 </main>
 <!-- ======================= Company Values ==================-->
   <section class="container-fluid p-4" id="company-values">
-    <div class="row justify-content-center">
+    <div class="row justify-content-center p-2">
       <h2>Company Values</h2>
     </div>
     <div class="row justify-content-around">
       <div class="col-md-3 text-center" id="value">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/environmental.png" alt="environmental impact" title="environmental impact"/>
-        <h4>Low environmental impact</h4>
+        <a href="https://www.countrychicpaint.com/pages/our-story" target="blank"><img src="<?php echo get_template_directory_uri(); ?>/images/environmental.png" alt="environmental impact" title="environmental impact"/></a>
+        <a href="https://www.countrychicpaint.com/pages/our-story" class="text-decoration-none text-reset" target="blank"><h4>Low environmental impact</h4></a>
       </div>
       <div class="col-md-3 text-center" id="value">
-        <img class="value" src="<?php echo get_template_directory_uri(); ?>/images/recycle.png" alt="recycle" title="recycle"/>
-        <h4>Participating in Recycling Programs</h4>
+        <a href="https://www.countrychicpaint.com/pages/our-story" target="blank"><img class="value" src="<?php echo get_template_directory_uri(); ?>/images/recycle.png" alt="recycle" title="recycle"/></a>
+        <a href="https://www.countrychicpaint.com/pages/our-story" class="text-decoration-none text-reset" target="blank"><h4>Participating in Recycling Programs</h4></a>
       </div>
       <div class="col-md-3 text-center" id="value">
-        <img class="value" src="<?php echo get_template_directory_uri(); ?>/images/house.png" alt="house" title="house"/>
-        <h4>Supporting the local economy</h4>
+        <a href="https://www.countrychicpaint.com/pages/our-story" target="blank"><img class="value" src="<?php echo get_template_directory_uri(); ?>/images/house.png" alt="house" title="house"/></a>
+        <a href="https://www.countrychicpaint.com/pages/our-story" class="text-decoration-none text-reset" target="blank"><h4>Supporting the local economy</h4></a>
       </div>
       <div class="col-md-3 text-center" id="value">
-        <img class="value"src="<?php echo get_template_directory_uri(); ?>/images/heart.png" alt="heart" title="heart"/>
-        <h4>Helping those in need</h4>
+        <a href="https://www.countrychicpaint.com/pages/our-story" target="blank"><img class="value"src="<?php echo get_template_directory_uri(); ?>/images/heart.png" alt="heart" title="heart"/></a>
+        <a href="https://www.countrychicpaint.com/pages/our-story" class="text-decoration-none text-reset" target="blank"><h4>Helping those in need</h4></a>
       </div>
     </div>
   </section>
