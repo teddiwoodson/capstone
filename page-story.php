93dce6d70922224dd02b229dfa711c4a0087b2a1
Teddi Woodson <teddiwood97@ufl.edu>
2020-04-11 16:54:48 -0400

    Edit content on Our Story Values

--- page-story.php
+++ page-story.php
@@ -15,27 +15,50 @@
   </section>
 </main>
 <!-- ======================= Company Values ==================-->
-  <section class="container-fluid p-4" id="company-values">
-    <div class="row justify-content-center p-2">
-      <h2>Company Values</h2>
+
+  <section class="container-fluid" id="learn-more">
+    <div class="row justify-content-center">
+      <div class="col-12">
+      <h2>Country Chic Paint Values</h2>
     </div>
-    <div class="row justify-content-around">
-      <div class="col-md-3 text-center value">
-        <a href="https://www.countrychicpaint.com/pages/our-story" target="blank"><img src="<?php echo get_template_directory_uri(); ?>/images/environmental.png" alt="environmental impact" title="environmental impact"/></a>
-        <a href="https://www.countrychicpaint.com/pages/our-story" class="text-decoration-none text-reset" target="blank"><h4>Low environmental impact</h4></a>
+    </div>
+      <div class="row d-flex justify-content-center">
+        <div class="col-lg-3 col-md-3 col-12 collapse-style">
+          <div>
+          <img  src="<?php echo get_template_directory_uri(); ?>/images/VOC-Icon-White.png" alt="VOC Free Icon" title="VOC Free Icon"/>
+          <h5>Lowering Environmental Impacts</h5>
+          <p><a class="btn btn-primary" data-toggle="collapse" href="#collapseExample1" role="button" aria-expanded="false" aria-controls="collapseExample">Read More</a></p>
+          <div class="collapse" id="collapseExample1">
+            <div class="card card-body">
+              Our products and pigments have low to zero VOC level, all printed materials use 100% recycled paper from sustainable sources, and our headquarters and warehouse use 98% renewable energy.
+            </div>
+            </div>
+            </div>
       </div>
-      <div class="col-md-3 text-center value">
-        <a href="https://www.countrychicpaint.com/pages/our-story" target="blank"><img src="<?php echo get_template_directory_uri(); ?>/images/recycle.png" alt="recycle" title="recycle"/></a>
-        <a href="https://www.countrychicpaint.com/pages/our-story" class="text-decoration-none text-reset" target="blank"><h4>Participating in Recycling Programs</h4></a>
+    <div class="col-lg-3 col-md-3 col-12 collapse-style">
+      <div>
+      <img  src="<?php echo get_template_directory_uri(); ?>/images/Economy-Icon.png" alt="Economy Icon" title="Economy Icon"/>
+      <h5>Supporting the local economy</h5>
+      <p><a class="btn btn-primary" data-toggle="collapse" href="#collapseExample3" role="button" aria-expanded="false" aria-controls="collapseExample">Read More</a></p>
+      <div class="collapse" id="collapseExample3">
+        <div class="card card-body">
+          Our products and raw materials are sourced and manufactured in North America whenever possible, supporting the sustainability of the local economy, in addition to leading innovation, productivity, and jobs.
+        </div>
+        </div>
+        </div>
+  </div>
+  <div class="col-lg-3 col-md-3 col-12 collapse-style">
+    <div>
+    <img  src="<?php echo get_template_directory_uri(); ?>/images/Heart-Icon.png" alt="Heart Icon" title="Heart Icon"/>
+    <h5>Giving Back to the community</h5>
+    <p><a class="btn btn-primary" data-toggle="collapse" href="#collapseExample4" role="button" aria-expanded="false" aria-controls="collapseExample">Read More</a></p>
+    <div class="collapse" id="collapseExample4">
+      <div class="card card-body">
+        We are proud to donate a portion of our profits to Food Banks Canada and Feeding America to help relieve hunger by providing meals to families in North America.
       </div>
-      <div class="col-md-3 text-center value">
-        <a href="https://www.countrychicpaint.com/pages/our-story" target="blank"><img src="<?php echo get_template_directory_uri(); ?>/images/house.png" alt="house" title="house"/></a>
-        <a href="https://www.countrychicpaint.com/pages/our-story" class="text-decoration-none text-reset" target="blank"><h4>Supporting the local economy</h4></a>
       </div>
-      <div class="col-md-3 text-center value">
-        <a href="https://www.countrychicpaint.com/pages/our-story" target="blank"><img src="<?php echo get_template_directory_uri(); ?>/images/heart.png" alt="heart" title="heart"/></a>
-        <a href="https://www.countrychicpaint.com/pages/our-story" class="text-decoration-none text-reset" target="blank"><h4>Helping those in need</h4></a>
       </div>
+</div>
     </div>
   </section>
 
