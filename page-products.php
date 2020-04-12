6f6600a3601d452edf29df922cfe0fb17ea8f5bd
ErikaJenkins <54445455+ErikaJenkins@users.noreply.github.com>
2020-04-12 12:06:00 -0400

    Update page-products.php

--- page-products.php
+++ page-products.php
@@ -15,6 +15,7 @@
   </section>
   </main>
 
+<<<<<<< master
 
   <!-- ======================= Our Impact  ==================-->
 
@@ -129,6 +130,63 @@
           </figure>
         </a>
       </div>
+=======
+<!-- ======================= Image Grid ==================-->
+  <section class="row justify-content-center">
+    <div class="content">
+      <div class="grid">
+        <figure class="effect-chico">
+            <img src="http://www.teddiwoodson.com/capstone/wp-content/uploads/2020/03/CCP_Group_OpenJars_edit-scaled-e1584509565904.jpg" alt="img15"/>
+            <figcaption>
+                <h2>All-In-One Paints</h2>
+                <!-- <p>Exerpt</p> -->
+                <a href="Link">View more</a>
+            </figcaption>
+        </figure>
+        <figure class="effect-chico">
+            <img src="http://www.teddiwoodson.com/capstone/wp-content/uploads/2020/03/CCP_OpenWaxJars_AllSizes-scaled.jpg" alt="img15"/>
+            <figcaption>
+                <h2>Waxes & Top Coats</h2>
+                <!-- <p>Exerpt</p> -->
+                <a href="Link">View more</a>
+            </figcaption>
+        </figure>
+        <figure class="effect-chico">
+            <img src="http://www.teddiwoodson.com/capstone/wp-content/uploads/2020/03/CCP_HempOil_Poured-scaled.jpg" alt="img15"/>
+            <figcaption>
+                <h2>Finishes</h2>
+                <!-- <p>Exerpt</p> -->
+                <a href="Link">View more</a>
+            </figcaption>
+        </figure>
+        <figure class="effect-chico">
+            <img src="http://www.teddiwoodson.com/capstone/wp-content/uploads/2020/03/CCP_Brushes_Group_Flatlay-scaled.jpg" alt="img15"/>
+            <figcaption>
+                <h2>Brushes & Sponges</h2>
+                <!-- <p>Exerpt</p> -->
+                <a href="Link">View more</a>
+            </figcaption>
+        </figure>
+        <figure class="effect-chico">
+            <img src="http://www.teddiwoodson.com/capstone/wp-content/uploads/2020/03/CCPgiveawayphoto-1-1-scaled.jpg" alt="img15"/>
+            <figcaption>
+                <h2>DIY Projects</h2>
+                <!-- <p>Exerpt</p> -->
+                <a href="Link">View more</a>
+            </figcaption>
+        </figure>
+        <figure class="effect-chico">
+            <img src="http://www.teddiwoodson.com/capstone/wp-content/uploads/2020/03/CCP_TigersEye_Dipped-scaled.jpg" alt="img15"/>
+
+            <figcaption>
+                <h2>Special Projects</h2>
+                <!-- <p>Exerpt</p> -->
+                <a href="Link">View more</a>
+            </figcaption>
+        </figure>
+      </div>
+    </div>
+>>>>>>> ErikaJenkins
   </section>
 
 <!-- ======================= Instagram Feed ==================-->
@@ -155,6 +213,7 @@
       <div class="row justify-content-center">
         <h2>Our Impact</h2>
       </div>
+<<<<<<< master
       <div class="row justify-content-around align-items-baseline">
         <div class="col-md-4">
           <div class="impact">
@@ -167,14 +226,35 @@
           <div class="impact">
             <img  src="<?php echo get_template_directory_uri(); ?>/images/recycle.png" alt="recycle" title="recycle"/>
             <p>Country Chic Paint is a proud member of paint recycling programs ensuring leftover paint is properly recycled.</p>
+=======
+      <div class="row ">
+        <div class="col-md-4 d-flex justify-content-center">
+          <div class="impact">
+            <img src="http://localhost:8888/wordpress/wp-content/uploads/2020/03/environmental.png" />
+            <p>Our products have zero to low VOC levels.</p>
+          </div>
+
+        </div>
+        <div class="col-md-4 d-flex justify-content-center">
+          <div class="impact">
+            <img src="http://localhost:8888/wordpress/wp-content/uploads/2020/03/recycle.png" />
+            <p>Our brochures and labels are printed on 100% recycled paper.</p>
+>>>>>>> ErikaJenkins
           </div>
 
 
         </div>
+<<<<<<< master
         <div class="col-md-4">
           <div class="impact">
             <img  src="<?php echo get_template_directory_uri(); ?>/images/renewable.png" alt="renewable" title="renewable"/>
             <p>Country Chic Paint’s headquarters and warehouse use 98% renewable energy.</p>
+=======
+        <div class="col-md-4 d-flex justify-content-center">
+          <div class="impact">
+            <img src="http://localhost:8888/wordpress/wp-content/uploads/2020/03/recycle.png" />
+            <p>Our headquarters and warehouse use 98% renewable energy.</p>
+>>>>>>> ErikaJenkins
         </div>
       </div>
     </div>
