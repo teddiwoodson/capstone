8ce2e20c3dd60408037727dc78ea7ff43d0dab00
Teddi Woodson <teddiwood97@ufl.edu>
2020-04-12 20:05:10 -0400

    Add Elizabeth's meet the team page

---
+++ page-meettheteam.php
@@ -0,0 +1,88 @@
+<?php
+/*
+    Template Name: meettheteam
+    Template Type: Page
+*/
+ ?>
+
+<?php get_header(); ?>
+
+<main>
+  <section class="row">
+    <div class="col-md-12">
+    <?php dynamic_sidebar('hero-image-contact'); ?>
+  </div>
+</section>
+<!-- ======================= Meet the Team ==================-->
+
+  <div class="row justify-content-center mt-4">
+    <div class="col-lg-3 col-md-6 col-sm-12 text-center m-2 justify-content-center">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/Rosanne.png" alt="Rosanne" title="Rosanne">
+      <h3>Hey! I'm Rosanne & I was once a ski instructor!</h3>
+      <p>If someone has a cure for my sweet tooth, you should really let me know as it's getting really out of hand.</p>
+      <p>Favorite color: <span style="color: #9bb7ac">Tropical Cocktail</span></p>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-12 text-center m-2 justify-content-center">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/Jan.png" alt="Jan" title="Jan">
+      <h3>Hi! I'm Jan & I'd love to roadtrip North America with my family!</h3>
+      <p>My best childhood memories are from helping my grandpa on his dairy farm. I love having lots of outdoorsy adventures with my kids.</p>
+      <p>Favorite color: <span style="color: #3b5764">Midnight Sky</span></p>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-12 text-center m-2 justify-content-center">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/Christa.png" alt="Christa" title="Christa">
+      <h3>Hi there! I'm Christa & I've hiked to the top of Mt. Kilimanjaro!</h3>
+      <p>Would you believe me if I told you that I once received a marriage proposal over our website's live chat?!</p>
+      <p>Favorite color: <span style="color: #5c6772">Hurricane</span></p>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-12 text-center m-2 justify-content-center">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/Laila-1.png" alt="Laila-1" title="Laila-1">
+      <h3>Hey! I'm Laila & I make friends with crows!</h3>
+      <p>With three kids in the house, I've taken to hiding my cookie stash in the frozen broccoli bag so no one will steal them.</p>
+      <p>Favorite color: <span style="color: #3a4427">Peacoat</span></p>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-12 text-center m-2 justify-content-center">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/Sarah.png" alt="Sarah" title="Sarah">
+      <h3>Hello! I'm Sarah & I can whistle and hum at the same time!</h3>
+      <p>On Sunday mornings you can find me curled up with my cat, Penelope (AKA Monkey), a blanket, and a cup of tea.</p>
+      <p>Favorite color: <span style="color: #d4c4b3">Happy Hour</span></p>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-12 text-center m-2 justify-content-center">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/Mary.png" alt="Mary" title="Mary">
+      <h3>Hi! I'm Mary & I speak Dothraki to my cat!</h3>
+      <p>My most challenging job here at Country Chic Paint is trying to keep the office plants alive.</p>
+      <p>Favorite color: <span style="color: #51663f">Jitterbug</span></p>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-12 text-center m-2 justify-content-center">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/Kelly.png" alt="Kelly" title="Kelly">
+      <h3>Hello! I'm Kelly and I was once crowned "Miss Chemainus"!</h3>
+      <p>My house is like a zoo. My daughter keeps bringing home new rescue animals and they never seem to leave.</p>
+      <p>Favorite color: <span style="color: #495f59">Hollow Hill</span></p>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-12 text-center m-2 justify-content-center">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/Karl.png" alt="Karl" title="Karl">
+      <h3>What's up? I'm Karl & I love exotic cars!</h3>
+      <p>The most satisfying part of my job is when a large bucket of paint pours perfectly into pint and quart jars with nothing left over.</p>
+      <p>Favorite color: <span style="color: #513736">Liquorice</span></p>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-12 text-center m-2 justify-content-center">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/Emilie.png" alt="Emilie" title="Emilie">
+      <h3>Hey! I'm Emelie & I hope to live in Greece someday!</h3>
+      <p>I was once attacked by a bird while working here at Country Chic Paint when I wandered a bit too close to its nest.</p>
+      <p>Favorite color: <span style="color: #76898e">Piece of Cake</span></p>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-12 text-center m-2 justify-content-center">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/Jessie.png" alt="Jessie" title="Jessie">
+      <h3>Hi! I'm Jessie & I once met JTT at a golf course!</h3>
+      <p>Every year I throw birthday parties for my two dogs, Max & Joey complete with party hats and cakes!</p>
+      <p>Favorite color: <span style="color: #97adbc">Mermaid's Tale</span></p>
+    </div>
+  </div>
+
+</main>
+
+
+
+
+
+
+<?php get_footer(); ?>
