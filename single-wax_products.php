35796258aff61f3978908ee366930a7dc080b82e
Teddi Woodson <teddiwood97@ufl.edu>
2020-04-15 22:16:44 -0400

    Update all single product pages to remove extra code

--- single-wax_products.php
+++ single-wax_products.php
@@ -1,35 +1,35 @@
 <?php get_header(); ?>
-  <div class="container">
-    <div>
-    <h3 class="product-entry-title"><?php the_title(); ?></h3>
-  </div>
+  <main class="container product-entry-text">
+    <section>
+      <h3 class="product-entry-spacing"><?php the_title(); ?></h3>
+    </section>
 
-    <div class="row justify-content-center single-product">
-      <main class="col-lg-5 col-md-6 col-12">
+    <section class="row justify-content-center product-entry-spacing">
+      <article class="col-lg-5 col-md-6 col-12">
         <div>
-        <?php
-          if(have_posts()) {
-            while(have_posts()){
-              the_post(); ?>
+          <?php
+            if(have_posts()) {
+              while(have_posts()){
+                the_post(); ?>
 
-            <div class="product-feature-img">
-              <?php the_post_thumbnail(); ?>
-            </div>
+              <div class="product-feature-img">
+                <?php the_post_thumbnail(); ?>
+              </div>
 
-            <?php dynamic_sidebar('shop-form'); ?>
+              <?php dynamic_sidebar('shop-form'); ?>
 
-          <?php  } //this ends while loop
-          }// this ends if statement
-        ?>
-      </div>
-      </main>
-      <section class="col-lg-7 col-md-6 col-12">
+            <?php  } //this ends while loop
+            }// this ends if statement
+          ?>
+        </div>
+      </article>
+      <article class="col-lg-7 col-md-6 col-12">
         <?php
           if(have_posts()) {
             while(have_posts()){
               the_post(); ?>
             <h5>Description</h5>
-            <div class="description">
+          <div class="description">
             <p><?php the_content();?></p>
           </div>
 
@@ -41,10 +41,8 @@
           <?php  } //this ends while loop
           }// this ends if statement
         ?>
-      </section>
+      </article>
+  </section>
 
-
-
-    </div>
-  </div>
+  </main>
 <?php get_footer(); ?>
