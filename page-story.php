69e3c3b6a020484ab47916156f5f18446d1a48ea
Teddi Woodson <teddiwood97@ufl.edu>
2020-04-16 22:27:39 -0400

    Update our story page

--- page-story.php
+++ page-story.php
@@ -7,36 +7,29 @@
 
 <?php get_header(); ?>
 <!-- ======================= Hero Image ==================-->
-<main class="container">
-  <section class="row">
-    <div class="col-md-12 justify-content-center">
-      <?php dynamic_sidebar('hero-image-story'); ?>
-    </div>
-  </section>
-</main>
+  <?php dynamic_sidebar('hero-image-story'); ?>
 <!-- ======================= Company Values ==================-->
 
-  <section class="container-fluid" id="learn-more">
-    <div class="row justify-content-center">
-      <div class="col-12">
-      <h2>Country Chic Paint Values</h2>
-    </div>
-    </div>
-      <div class="row d-flex justify-content-center">
-        <div class="col-lg-3 col-md-3 col-12 collapse-style">
-          <div>
-          <img  src="<?php echo get_template_directory_uri(); ?>/images/VOC-Icon-White.png" alt="VOC Free Icon" title="VOC Free Icon"/>
-          <h5>Lowering Environmental Impacts</h5>
-          <p><a class="btn btn-primary" data-toggle="collapse" href="#collapseExample1" role="button" aria-expanded="false" aria-controls="collapseExample">Read More</a></p>
-          <div class="collapse" id="collapseExample1">
-            <div class="card card-body">
+  <main class="container-fluid" id="learn-more">
+    <section class="row justify-content-center">
+      <article class="col-12">
+        <h2>Country Chic Paint Values</h2>
+      </article>
+    </section>
+    <section class="row d-flex justify-content-center">
+      <article class="col-lg-3 col-md-3 col-12 collapse-style">
+        <img  src="<?php echo get_template_directory_uri(); ?>/images/VOC-Icon-White.png" alt="VOC Free Icon" title="VOC Free Icon"/>
+        <h5>Lowering Environmental Impacts</h5>
+        <p><a class="btn btn-primary" data-toggle="collapse" href="#collapseExample1" role="button" aria-expanded="false" aria-controls="collapseExample">Read More</a></p>
+        <div class="collapse" id="collapseExample1">
+          <div class="card card-body">
               Our products and pigments have low to zero VOC level, all printed materials use 100% recycled paper from sustainable sources, and our headquarters and warehouse use 98% renewable energy.
-            </div>
-            </div>
-            </div>
-      </div>
-    <div class="col-lg-3 col-md-3 col-12 collapse-style">
-      <div>
+          </div>
+        </div>
+      </article>
+    </section>
+    <section class="col-lg-3 col-md-3 col-12 collapse-style">
+      <article>
       <img  src="<?php echo get_template_directory_uri(); ?>/images/Economy-Icon.png" alt="Economy Icon" title="Economy Icon"/>
       <h5>Supporting the local economy</h5>
       <p><a class="btn btn-primary" data-toggle="collapse" href="#collapseExample3" role="button" aria-expanded="false" aria-controls="collapseExample">Read More</a></p>
@@ -45,10 +38,10 @@
           Our products and raw materials are sourced and manufactured in North America whenever possible, supporting the sustainability of the local economy, in addition to leading innovation, productivity, and jobs.
         </div>
         </div>
-        </div>
-  </div>
-  <div class="col-lg-3 col-md-3 col-12 collapse-style">
-    <div>
+      </article>
+  </section>
+  <section class="col-lg-3 col-md-3 col-12 collapse-style">
+    <article>
     <img  src="<?php echo get_template_directory_uri(); ?>/images/Heart-Icon.png" alt="Heart Icon" title="Heart Icon"/>
     <h5>Giving Back to the community</h5>
     <p><a class="btn btn-primary" data-toggle="collapse" href="#collapseExample4" role="button" aria-expanded="false" aria-controls="collapseExample">Read More</a></p>
@@ -57,24 +50,23 @@
         We are proud to donate a portion of our profits to Food Banks Canada and Feeding America to help relieve hunger by providing meals to families in North America.
       </div>
       </div>
-      </div>
-</div>
-    </div>
-  </section>
+    </article>
+</section>
+  </main>
 
 <!-- ======================= A Family Affair ==================-->
 <section class="container">
-  <section class="row">
-    <div class="col-md-12 justify-content-center">
+  <div class="row">
+    <article class="col-md-12 justify-content-center">
       <?php dynamic_sidebar('story-widget'); ?>
-    </div>
-  </section>
+    </article>
+  </div>
 
 <!-- ======================= Meet the Team ==================-->
-<section class="meet-team">
+<article class="meet-team">
     <h2>Meet The Team</h2>
     <a href="http://www.teddiwoodson.com/capstone/meet-the-team/"><?php echo do_shortcode('[sp_wpcarousel id="59"]'); ?></a>
-</section>
+</article>
 </section>
 
 
