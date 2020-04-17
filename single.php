03c519cd148e99b845e86880b10a72ea29286fe2
Teddi Woodson <teddiwood97@ufl.edu>
2020-04-16 22:16:53 -0400

    Update single.php page

--- single.php
+++ single.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
-  <div class="container">
-    <div class="row justify-content-center" id="posts">
-      <main class="col-md-9">
+  <main class="container">
+    <section class="row justify-content-center" id="posts">
+      <article class="col-md-9">
         <?php
           if(have_posts()) {
             while(have_posts()){
@@ -14,21 +14,21 @@
           }// this ends if statement
         ?>
 
-      </main>
+      </article>
 
       <aside class="col-md-3">
         <?php get_sidebar(); ?>
       </aside>
-    </div>
-  </div>
-  <div class="container-fluid" id="learn-more">
+    </section>
+  </main>
+  <section class="container-fluid" id="learn-more">
     <h2>Check out some of our other Tutorials</h2>
-    <div class="row justify-content-center">
+    <article class="row justify-content-center">
       <div class="col-10">
         <?php dynamic_sidebar('recent-posts'); ?>
       </div>
-    </div>
+    </artciel>
 
-  </div>
+  </section>
 
 <?php get_footer(); ?>
