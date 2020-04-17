dbf7ab14b3b7ef83fcd6249bd674d82c55f702b4
Teddi Woodson <teddiwood97@ufl.edu>
2020-04-16 20:39:04 -0400

    Update 404 code

--- 404.php
+++ 404.php
@@ -1,17 +1,15 @@
 <?php get_header(); ?>
 
-  <div class="container">
-    <div class="row" id="oops">
-      <main class="col-md-12 oops">
+  <main class="container heading">
+    <section class="row" id="oops">
+      <article class="col-md-12">
         <h2>Oops. Page not found.</h2>
         <p>I'm sorry, this never really happens. It looks like nothing was found at this location.</p>
         <img  src="<?php echo get_template_directory_uri(); ?>/images/oops.gif" alt="404 gif" title="404 gif"/>
         <p>Let's start over.</p>
-      </main>
-      </div>
-      <section class="row justify-content-center">
         <a href="<?php echo home_url('/') ?>" type="button" class="btn btn-primary btn-lg">Return Home</a>
-      </section>
-  </div>
+      </article>
+    </section>
+  </main>
 
 <?php get_footer(); ?>
